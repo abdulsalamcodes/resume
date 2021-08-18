4fcb145822e1151179fdecf5d0d942a45ea88592
abdulsalamcodes <abdulsalamabodunrin369@gmail.com>
2021-08-18 09:40:46 +0100

    feat: add required links

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b28890395d8b38b0e88c"; }
+/******/ 		__webpack_require__.h = function() { return "637af81534ba21033b79"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
