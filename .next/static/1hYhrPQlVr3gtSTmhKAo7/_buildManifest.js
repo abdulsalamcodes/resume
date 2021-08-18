1800e58b719e393560bade255230719517cb0598
abdulsalamcodes <abdulsalamabodunrin369@gmail.com>
2021-08-18 09:58:48 +0100

    fix: eslint error

---
+++ .next/static/1hYhrPQlVr3gtSTmhKAo7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/134abd4c33ebf5674bd2.css","static/chunks/pages/index-3a4dfc1921c87b8b4f89.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
